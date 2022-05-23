7980412ed2fddd7e673757f9dc76ceee98c7544d
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-05-23 18:28:33 +0200

    modif runFx script

--- build/build_run.ps1
+++ build/build_run.ps1
@@ -5,3 +5,5 @@ $build = "C:\Users\noahm\OneDrive\Documents\tests\java\NomNomGenerator\build\"
 #}
 javac .\ch\*.java .\ch\enums\*.java .\ch\GraphContent\*.java -d .\build
 java --enable-preview -XX:+ShowCodeDetailsInExceptionMessages -cp .\build 'ch.Main' $args[0] 
+
+#java '-Dprism.order=sw' -Xmx8192m -Xms8192m --enable-preview '-XX:+ShowCodeDetailsInExceptionMessages' --module-path $env:JAVAFX_HOME --add-modules javafx.controls,javafx.fxml,javafx.graphics  '@C:\Users\noahm\AppData\Local\Temp\cp_bk0kgri6ijybhcodh9e0g76g5.argfile' 'ch.Main'
--- runFxJar.ps1
+++ runFxJar.ps1
@@ -1,1 +1,4 @@
-java '-Dprism.order=sw' -Xmx8192m -Xms8192m --enable-preview '-XX:+ShowCodeDetailsInExceptionMessages' --module-path $env:JAVAFX_HOME --add-modules javafx.controls,javafx.fxml,javafx.graphics -jar  NomNomGenerator.jar
+$jarPath = NomNomGenerator.jar
+if ($args.Length > 0) { $jarPath = $args[0] }
+
+java '-Dprism.order=sw' -Xmx8192m -Xms8192m --enable-preview '-XX:+ShowCodeDetailsInExceptionMessages' --module-path $env:JAVAFX_HOME --add-modules javafx.controls,javafx.fxml,javafx.graphics -jar  $jarPath
