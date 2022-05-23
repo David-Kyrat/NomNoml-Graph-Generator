4be1af4e66f2581e0803a10b21128e52f52f9b1c
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-05-23 18:43:28 +0200

    modif runFx script

--- runFxJar.ps1
+++ runFxJar.ps1
@@ -1,4 +1,7 @@
-$jarPath = NomNomGenerator.jar
-if ($args.Length > 0) { $jarPath = $args[0] }
+$oldPath = pwd
+$arg = @($args)[0]
 
-java '-Dprism.order=sw' -Xmx8192m -Xms8192m --enable-preview '-XX:+ShowCodeDetailsInExceptionMessages' --module-path $env:JAVAFX_HOME --add-modules javafx.controls,javafx.fxml,javafx.graphics -jar  $jarPath
+if ($arg -ne $null) {cd $arg}
+java '-Dprism.order=sw' -Xmx8192m -Xms8192m --enable-preview '-XX:+ShowCodeDetailsInExceptionMessages' --module-path $env:JAVAFX_HOME --add-modules javafx.controls,javafx.fxml,javafx.graphics -jar "NomNomGenerator.jar"
+
+if ($arg -ne $null) {cd $oldPath}
